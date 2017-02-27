d95ca77238fca303c40848efbfc9b4d76f4d8393
narendra_k <narendra@hashtaag.com>
2017-02-27 17:25:03 +0530

    first commit

---
+++ app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<p>Thi sis the pragraph of angular2 for trainee </p> <h2>this is my first angular2 practices sessionp</h2>'
+})
+
+export class AppComponent {
+
+}
