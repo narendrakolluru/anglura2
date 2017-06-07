37e02d7779d40ab5e36dfae14e982bcc50918325
JPA Suresh <jpas1223@gmail.com>
2017-06-07 13:50:24 +0530

    initial testing commit
    New commit for creating folder structure testing

---
+++ app/components/home.component.js
@@ -0,0 +1,19 @@
+  (function(app) {
+  app.homeComponent =
+    ng.core.Component({
+      template: '<div>Angular2-js</div>'
+    })
+    .Class({
+      constructor:function() {
+        
+
+      
+      },
+      ngOnInit: function(){
+        
+      }
+
+    });
+})(window.app || (window.app = {}));
+
+
